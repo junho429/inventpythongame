72d8707cac55653ee0794ddddd25fe17fd3d99cd
서준호 <junho429@naver.com>
2018-04-10 15:01:21 +0900

    행맨 코드 구현

--- hangman.py
+++ hangman.py
@@ -39,25 +39,92 @@ HANGMAN_PICS = ['''
 
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
-
 def getRandomWord(wordList):
-    #이 함수는 문자열의 리스트에서 랜덤한 문자를 반환합니다.
-    wordIndex = random.randist(0,len(wordList) - 1)
+    wordIndex = random.randint(0, len(wordList) -1)
     return wordList[wordIndex]
 
-def displayBoard(missedLetters, correntLetters, secretWord):
+def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
     print()
-
-    print('Missed letters:', end='    ')
+#맞추지 못한 단어를 for문 돌아가면서 하나하나 표시합니다.
+    print('Missed letters:', end=' ')
     for letter in missedLetters:
         print(letter, end=' ')
     print()
-
+#먼저 모든 칸을 _______로 표시합니다.
     blanks = '_' * len(secretWord)
-
+#혹시 맞춘 단어가 있을 경우 _를 맞춘 단어로 교체합니다.
     for i in range(len(secretWord)):
         if secretWord[i] in correctLetters:
-            blanks = blanks[:i] + secretWord[i] + blanks[i + 1:]
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+#단어에  띄 어 쓰 기 를 넣어서 출력합니다. 
     for letter in blanks:
+        print(letter, end=' ')
+    print()
+
+#한 글자를 입력받아 유효한 값인지 확인합니다. 만약 유효한 값이 아니면 다시 입력받습니다.
+def getGuess(alreadyGuessed):
+    while True:
+        print('Guess a letter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Pleace enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Pleace enter a LETTER.')
+        else:
+            return guess
+#플레이어가 다시 플레이할지 확인합니다.
+def playAgain():
+    print('Do you want to play again (yes or no)')
+    return input().lower().startswith('y')
+
+print('H A N G M A N')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+        
+        #플레이어가 이겼는지 확인
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+
+        if foundAllLetters:
+            print('Yes! The secret word is "' + secretWord + '"! You have won!')
+            gameIsDone = True
+
+    else:
+        missedLetters = missedLetters + guess
+
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord)
+            print('you have run out of guesses! \n After ' +
+                  str(len(missedLetters))+ ' missed guesses and ' +
+                  str(len(correctLetters)) + ' correct guesses, the word was"'
+                  + secretWord + '"')
+            gameIsDone = True
+
+
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord = getRandomWord(words)
+        else:
+            break
+            
         
